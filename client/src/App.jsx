9c4cba5e079347cc50782f277d2d182ea4a20a38
Arafin Mridul <mridularafin@gmail.com>
2024-01-13 18:55:06 +0600

    necessary deps and files

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useState } from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import "./App.css";
+
+const App = () => {
+    return (
+        <div>
+            {" "}
+            Broke people can use this messenger to sell their kidneys lol{" "}
+        </div>
+    );
+};
+
+export default App;
