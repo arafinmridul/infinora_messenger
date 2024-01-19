3ed93b148d0da860959a2d2121d8dc0bdd19e4a8
Arafin Mridul <mridularafin@gmail.com>
2024-01-19 18:16:23 +0600

    auth route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,15 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const authRoutes = require("./routes/auth.js");
+
 require("dotenv").config();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
